94fe13fbd07d887f94b1cf5d096533d2797ba04f
oalaor <oalaorneto@gmail.com>
2023-07-03 15:59:50 -0300

    Revert "Revert "Merge branch 'developing' of https://github.com/Lasca101/SiteLearnify into developing""
    This reverts commit ca04acc3e78043cbe2624df12b02bcb7f8b8f156.

--- pagina-montar-curso/script_montar_curso.js
+++ pagina-montar-curso/script_montar_curso.js
@@ -1,103 +1,79 @@
 window.addEventListener('load', salvarAulas);
+window.addEventListener('load', loadVideo);
 
-document.querySelector('#botao-enviar-numero-aulas').onclick = () => {
-    let numeroAulas = document.querySelector('#input-inserir-numero-aulas').value;
-    let htmlInserirAulas = '';
-    let divAulas = document.querySelector('#div-inserir-img-link');
-    let htmlInserirBotaoFinalizar = '';
-    let divBotaoFinalizar = document.querySelector('#div-botao-finalizar');
+function loadVideo() {
+    fetch('/json-teste/json.json')
+        .then(res => res.json())
+        .then(dados => {
 
-    for (let i = 0; i < numeroAulas; i++) {
-        htmlInserirAulas += `
-        <div class="inserir-aula">
-            <h3>${i + 1}º Video</h3>
-            <input id="inserir-titulo${i}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Insira o titulo do video">
-            <div class="div-inserir-img">
-                <label for="formFile" class="form-label">Selecione uma imagem para a thumbnail do video</label>
-                <input id="inputFile${i}" class="form-control form-control-" id="formFile" type="file">
-            </div>
-            <div class="form-floating mb-3 small div-inserir-link">
-                <input id="inputText${i}" class="form-control input-link" id="floatingInput" placeholder="">
-                <label for="floatingInput">Insira código do link do video</label>
-                <a href="#" class="botao-como-pegar-link">Como pegar o código do link?</a>
-            </div>
-        </div>`
-    }
-    htmlInserirBotaoFinalizar += `
-    <button id="finalizar-curso" type="button" class="btn btn-primary">Finalizar</button>`
+            let strDados = localStorage.getItem('db');
+            let objDados = {};
 
-    divAulas.innerHTML = htmlInserirAulas;
-    divBotaoFinalizar.innerHTML = htmlInserirBotaoFinalizar;
+            if (strDados) {
+                objDados = JSON.parse(strDados);
+            }
+            else {
+                objDados = dados;
+            }
 
-    document.querySelector('#finalizar-curso').onclick = () => {
-        fetch('/json-teste/json.json')
-            .then(res => res.json())
-            .then(dados => {
+            localStorage.setItem('db', JSON.stringify(objDados));
 
-                let strDados = localStorage.getItem('db');
-                let objDados = {};
-                if (strDados) {
-                    objDados = JSON.parse(strDados);
-                }
-                else {
-                    objDados = dados;
-                }
-                localStorage.setItem('db', JSON.stringify(objDados));
+            // Função para converter a imagem em base64
+            function convertToBase64(file, callback) {
+                const reader = new FileReader();
+                reader.onloadend = function () {
+                    callback(reader.result);
+                };
+                reader.readAsDataURL(file);
+            }
 
+            // Elementos HTML
+            let inputFileThumb = document.querySelector('#input-file-thumb');
+            let codigoLink = document.querySelector('#input-link-video').value;
+            let tituloVideo = document.querySelector('#inserir-titulo').value;
+            let idV = objDados.curso[objDados.curso.length - 1].video.length;
+            const adicionarButton = document.getElementById('finalizar-curso');
 
-                // Função para converter a imagem em base64
-                function convertToBase64(file, callback) {
-                    const reader = new FileReader();
-                    reader.onloadend = function () {
-                        callback(reader.result);
-                    };
-                    reader.readAsDataURL(file);
-                }
+            // Variáveis para armazenar os valores em base64
+            let imageBase64_1 = null;
 
-                for (let i = 0; i < numeroAulas; i++) {
-                    // Elementos HTML
-                    let inputFileThumb = document.querySelector(`#inputFile${i}`);
-                    let codigoLink = document.querySelector(`#inputText${i}`).value;
-                    let tituloVideo = document.querySelector(`#inserir-titulo${i}`).value;
+            // Evento change do input file 1
+            inputFileThumb.addEventListener('change', function (event) {
+                const file = event.target.files[0];
 
-                    // Variáveis para armazenar os valores em base64
-                    let imageBase64_1 = null;
-
-                    // Evento change do input file 1
-                    inputFileThumb.addEventListener('change', function (event) {
-                        const file = event.target.files[0];
-
-                        // Verifica se um arquivo foi selecionado
-                        if (file) {
-                            convertToBase64(file, function (base64) {
-                                // Armazena o valor em uma variável
-                                imageBase64_1 = base64;
-                            });
-                        }
+                // Verifica se um arquivo foi selecionado
+                if (file) {
+                    convertToBase64(file, function (base64) {
+                        // Armazena o valor em uma variável
+                        imageBase64_1 = base64;
                     });
-
-                    // Verifica se as duas imagens foram selecionadas
-                    if (imageBase64_1 && codigoLink && tituloVideo) {
-                        // Insere os valores em um objeto JSON
-                        let novoVideo = {
-                            idVideo: i,
-                            link: codigoLink,
-                            thumbnail: imageBase64_1,
-                            titulo: tituloVideo,
-                            data: [
-                            ]
-                        };
-
-                        objDados.curso[objDados.curso.length - 1].video.push(novoVideo);
-                        localStorage.setItem('db', JSON.stringify(objDados));
-                    } else {
-                        console.log('Por favor, selecione e preencha todos os campos.');
-                    }
-
                 }
-            })
-    }
+            });
+
+            adicionarButton.addEventListener('click', function () {
+                // Verifica se as duas imagens foram selecionadas
+                if (imageBase64_1 && codigoLink && tituloVideo) {
+                    // Insere os valores em um objeto JSON
+                    let novoVideo = {
+                        idVideo: idV,
+                        link: codigoLink,
+                        thumbnail: imageBase64_1,
+                        titulo: tituloVideo,
+                        data: [
+                        ]
+                    };
+
+                    objDados.curso[objDados.curso.length - 1].video.push(novoVideo);
+                    localStorage.setItem('db', JSON.stringify(objDados));
+                } else {
+                    console.log('Por favor, selecione e preencha todos os campos.');
+                }
+            });
+
+
+        })
 }
+
 
 
 
@@ -186,8 +162,9 @@ function salvarAulas() {
                     objDados.curso.push(novoCurso);
                     localStorage.setItem('db', JSON.stringify(objDados));
 
-                    document.querySelector('#div-numero-aulas').style.display = 'flex';
+                    document.querySelector('#div-inserir-img-link').style.display = 'flex';
                     document.querySelector('#continuar-cadastro').style.display = 'none';
+                    document.querySelector('#div-botao-finalizar').style.display = 'flex';
                 } else {
                     console.log('Por favor, selecione e preencha todos os campos.');
                 }
